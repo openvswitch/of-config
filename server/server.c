0d8246c9932115fd9510aa23a19943150d71195d
Tomáš Čejka <tomas@kassoft.eu>
2015-01-29 17:51:43 +0100

    server: use macros with paths
    add missing m4 macro for dir definitions

--- server/server.c
+++ server/server.c
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+#include <config.h>
+
 #include <errno.h>
 #include <getopt.h>
 #include <signal.h>
@@ -27,8 +29,15 @@
 
 #include "common.h"
 
-/* TODO: move this into configure */
-#define CONFDIR "/etc/ofconfig/"
+#ifndef OFCONFIG_CONFDIR
+#define OFCONFIG_CONFDIR "/etc/ofconfig/"
+#warning "OFCONFIG_CONFDIR undefined by configure - this is unexpected"
+#endif
+
+#ifndef OFCONFIG_DATADIR
+#define OFCONFIG_DATADIR "/etc/ofconfig/"
+#warning "OFCONFIG_DATADIR undefined by configure - this is unexpected"
+#endif
 
 /* ietf-netconf-server transAPI structure from netconf-server-transapi.c */
 extern struct transapi server_transapi;
@@ -199,9 +208,9 @@ main(int argc, char **argv)
 #endif
 
     /* prepare the ietf-netconf-server module */
-    ncds_add_model(CONFDIR "/ietf-netconf-server/ietf-x509-cert-to-name.yin");
+    ncds_add_model(OFCONFIG_DATADIR "/ietf-netconf-server/ietf-x509-cert-to-name.yin");
     ds.server = ncds_new_transapi_static(NCDS_TYPE_FILE,
-                                         CONFDIR
+                                         OFCONFIG_DATADIR
                                          "/ietf-netconf-server/ietf-netconf-server.yin",
                                          &server_transapi);
     if (ds.server == NULL) {
@@ -210,7 +219,7 @@ main(int argc, char **argv)
         goto cleanup;
     }
     ncds_file_set_path(ds.server,
-                       CONFDIR "/ietf-netconf-server/datastore.xml");
+                       OFCONFIG_DATADIR "/ietf-netconf-server/datastore.xml");
     ncds_feature_enable("ietf-netconf-server", "ssh");
     ncds_feature_enable("ietf-netconf-server", "inbound-ssh");
     if ((ds.server_id = ncds_init(ds.server)) < 0) {
@@ -223,7 +232,7 @@ main(int argc, char **argv)
 
     /* prepare the of-config module */
     ds.ofc = ncds_new_transapi_static(NCDS_TYPE_CUSTOM,
-                                      CONFDIR "/of-config/of-config.yin",
+                                      OFCONFIG_DATADIR "/of-config/of-config.yin",
                                       &ofc_transapi);
     if (ds.ofc == NULL) {
         retval = EXIT_FAILURE;

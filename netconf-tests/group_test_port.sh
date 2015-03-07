01e91998630551139ae72367c09b474bac086412
Tomas Cejka <cejkat@cesnet.cz>
2015-03-07 01:10:21 +0100

    tests: renamed create&modify&remove scripts

--- netconf-tests/group_test_port.sh
+++ netconf-tests/group_test_port.sh
@@ -3,15 +3,10 @@
 ./reset.sh
 ./get-config.sh > start_state
 ./create_port_eth1.sh
-./create_remove_tunnel.sh
-./create_remove_queue.sh
+./create_modify_remove_tunnel.sh
+./create_modify_remove_queue.sh
 ./openflow_set.sh
 ./remove_port_eth1.sh
-./get-config.sh | diff start_state -
-if [ $? -eq 0 ]; then
-    echo "Create and remove ended successfully."
-else
-    echo "Test failed."
-fi
+check_startup_difference start_state
 
 rm start_state

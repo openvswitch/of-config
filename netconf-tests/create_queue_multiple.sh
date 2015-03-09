d7329176ef86e59ce3bfa10fef0b832a5587759e
Tomas Cejka <cejkat@cesnet.cz>
2015-03-09 23:00:03 +0100

    tests: enhancement of test of multiple queues and ports

--- netconf-tests/create_queue_multiple.sh
+++ netconf-tests/create_queue_multiple.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 . ./config
 
+# COUNT must be at least 2!!!
 COUNT=4
+
+if [ "$COUNT" -lt 2 ]; then
+    exit 1
+fi
 
 KEYS=`seq 1 $COUNT`
 
@@ -50,7 +55,57 @@ done;)
 ./run_edit_config.sh $LIST running
 
 check_startup_difference start_state
+
+echo "Multiple queues on multiple ports"
+LIST=$(for i in $KEYS; do
+    sed "s,eth1,eth$i," create_port_eth1.xml > tempxml$i
+    echo tempxml$i
+    for q in $KEYS; do
+        sed "s,<port>ofc-bridge</port>,<port>eth$i</port>,;
+             s/666/$q/;
+             s/my_queue/q-$q-port-$i/" create_queue.xml > tempxmlq$i$q
+        echo tempxmlq$i$q
+    done;
+done;)
+./run_edit_config.sh $LIST running
 ./get-config.sh
+
+echo "Change multiple queues on multiple ports"
+LIST=$(for i in $KEYS; do
+    for q in $KEYS; do
+        sed "s,<port>ofc-bridge</port>,<port>eth$i</port>,;
+             s/667/$((i * 100 + q))/;
+             s/my_queue/q-$q-port-$i/" change_queue.xml > tempxml$i$q
+        echo tempxml$i$q
+    done;
+done;)
+./run_edit_config.sh $LIST running
+./get-config.sh
+
+echo "Try to move all queues from 2nd port to 1st"
+LIST=$(for q in $KEYS; do
+        sed "s/my_queue/q-$q-port-2/" change_queue_port.xml > tempxml$q
+        echo tempxml$q
+done;)
+./run_edit_config.sh $LIST running
+./get-config.sh
+
+
+echo "Remove multiple queues on multiple ports"
+LIST=$(for i in $KEYS; do
+    for q in $KEYS; do
+        sed "s,<port>ofc-bridge</port>,<port>eth$i</port>,;
+             s/my_queue/q-$q-port-$i/" remove_queue.xml > tempxml$i$q
+        echo tempxml$i$q
+    done;
+done;)
+./run_edit_config.sh $LIST running
+LIST=$(for i in $KEYS; do
+  sed "s,eth1,eth$i," remove_port_eth1.xml > tempxml$i
+  echo tempxml$i
+done;)
+./run_edit_config.sh $LIST running
+check_startup_difference start_state
 
 rm -f tempxml*
 

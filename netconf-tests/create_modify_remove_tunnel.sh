d2e5bec01cd49ed37a6f6e69ab992fc2a30899ea
Tomas Cejka <cejkat@cesnet.cz>
2015-03-13 15:47:36 +0100

    tests: add vxlan-tunnel

--- netconf-tests/create_modify_remove_tunnel.sh
+++ netconf-tests/create_modify_remove_tunnel.sh
@@ -6,7 +6,10 @@ connect --login $USER $HOST
 edit-config --config=create_tunnel_port.xml running
 get-config --filter=ovs.xml running
 
-edit-config --config=change_tunnel.xml running
+edit-config --config=change_ipgretunnel.xml running
+get-config --filter=ovs.xml running
+
+edit-config --config=remove_tunnel.xml running
 get-config --filter=ovs.xml running
 
 edit-config --config=remove_tunnel.xml running
